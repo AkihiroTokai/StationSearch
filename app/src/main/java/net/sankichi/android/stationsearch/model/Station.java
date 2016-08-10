1ebaf1733c2953dd2bc7158939eb49f3e5663273
Sankichi <sankichi92@sankichi.net>
2016-08-11 08:30:11 +0900

    APIに対応したStationモデルの作成

---
+++ app/src/main/java/net/sankichi/android/stationsearch/model/Station.java
@@ -0,0 +1,14 @@
+package net.sankichi.android.stationsearch.model;
+
+public class Station {
+    public String name;
+    public String kana;
+    public String prev;
+    public String next;
+    public double x;
+    public double y;
+    public double distance;
+    public String postal;
+    public String prefecture;
+    public String line;
+}
