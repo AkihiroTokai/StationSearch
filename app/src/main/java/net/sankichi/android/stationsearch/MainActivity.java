2c8a6f6c88a41ddce48238f444b17a41f0c31b9f
Sankichi <sankichi92@sankichi.net>
2016-08-11 08:28:40 +0900

    ビューの関連付け

--- app/src/main/java/net/sankichi/android/stationsearch/MainActivity.java
+++ app/src/main/java/net/sankichi/android/stationsearch/MainActivity.java
@@ -1,13 +1,32 @@
 package net.sankichi.android.stationsearch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView lineText;
+    private TextView kanaText;
+    private TextView nameText;
+    private TextView distanceText;
+    private TextView prevText;
+    private TextView nextText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.edit_text);
+        lineText = (TextView) findViewById(R.id.line_text);
+        kanaText = (TextView) findViewById(R.id.kana_text);
+        nameText = (TextView) findViewById(R.id.name_text);
+        distanceText = (TextView) findViewById(R.id.distance_text);
+        prevText = (TextView) findViewById(R.id.prev_text);
+        nextText = (TextView) findViewById(R.id.next_text);
     }
 }
